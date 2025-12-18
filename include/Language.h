6d839dce821968910b70ad28cf0ab0419c237e1b
Vladimir Pyshkin <vladimirpyshkin2007@gmail.com>
2025-12-18 10:10:22 +0300

    Add sqrt return main comma to Language.h

--- include/Language.h
+++ include/Language.h
@@ -1,5 +1,5 @@
-#ifndef OPERATION_H
-#define OPERATION_H
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
 
 enum IsCustomOp {
     PseudoOp    = -1,
@@ -21,18 +21,21 @@ enum NumberOfParams {
     macros( "*",       OP_MUL,          NonCustomOp, 0, "*"      ) \
     macros( "/",       OP_DIV,          NonCustomOp, 0, "/"      ) \
     macros( "^",       OP_POW,          NonCustomOp, 0, "^"      ) \
+    macros( "sqrt",    OP_SQRT,         NonCustomOp, 0, "sqrt"   ) \
     macros( "input",   OP_IN,           NonCustomOp, 0, "input"  ) \
-    macros( "output",  OP_OUT,          CustomOp,    1, "output" ) \
+    macros( "print",   OP_OUT,          CustomOp,    1, "print"  ) \
     macros( "if",      OP_IF,           CustomOp,    1, "if"     ) \
     macros( "else",    OP_ELSE,         NonCustomOp, 0, "else"   ) \
     macros( "while",   OP_WHILE,        CustomOp,    1, "while"  ) \
     macros( "func",    OP_FUNC,         NonCustomOp, 0, "func"   ) \
     macros( "call",    OP_CALL,         NonCustomOp, 0, "call"   ) \
+    macros( "return",  OP_RETURN,       NonCustomOp, 0, "return" ) \
+    macros( "main",    OP_MAIN,         NonCustomOp, 0, "main"   ) \
+    macros( ",",       OP_COMMA,        PseudoOp,    0, ","      ) \
+    macros( ";",       OP_SEMICOLON,    PseudoOp,    0, ";"      ) \
     macros( "(",       OP_OPEN_PARENT,  PseudoOp,    0, "("      ) \
     macros( ")",       OP_CLOSE_PARENT, PseudoOp,    0, ")"      ) \
     macros( "{",       OP_OPEN_BRACE,   PseudoOp,    0, "{"      ) \
-    macros( "}",       OP_CLOSE_BRACE,  PseudoOp,    0, "}"      ) \
-    macros( ";",       OP_SEMICOLON,    PseudoOp,    0, ";"      )
+    macros( "}",       OP_CLOSE_BRACE,  PseudoOp,    0, "}"      )
 
-    
 #endif
